f52997c9361f787c44a4cd80b807e1437e7e27c2
nona <iKonnyaku40@yahoo.co.jp>
2018-09-18 01:07:49 -0400

    coding

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Hello, World!")
+import queue
+
+
+class Process:
+
+    def __init__(self, name, processTime):
+        self.name = name
+        self.processTime = processTime
+        
+
+with open('test.txt','r') as file:
+    ele = file.readline().split()
+    eleNum = int(ele[0])
+    quantum = int(ele[1])
+
+    que = queue.Queue() 
+    for i in range(0, eleNum):
+        stringArray = file.readline().split()
+        process = Process(stringArray[0], int(stringArray[1]))
+        que.put(process)
+
+    CPUTime = 0
+    while que.qsize() > 0:
+        process = que.get()
+        CPUTime += quantum
+        process.processTime -= quantum
+        if process.processTime > 0:
+            que.put(process)
+            continue
+
+        CPUTime += process.processTime
+        print("{0} {1}".format(process.name, CPUTime))
