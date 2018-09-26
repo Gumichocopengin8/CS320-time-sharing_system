766d3f316a9a38a59c0f073787b5d2b8471edda9
unWyse <43249343+unWyse@users.noreply.github.com>
2018-09-26 13:18:55 -0400

    completed code
    probably needs more documentation

---
+++ main-final.py
@@ -0,0 +1,52 @@
+# Keita Nonaka
+# Nick Smith
+# Kyle Wyse
+# Koki Omori
+# 30 Sep 2018
+# OS Simulation
+
+# Project Description: 
+#
+#
+
+
+import queue
+
+
+class Process:
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
+
+output = ''
+
+with open('test.txt','r') as file:
+    ele = file.readline().split()
+    eleNum = int(ele[0]) # number of process
+    TASK_TIME_LIMIT = int(ele[1]) 
+
+    tasks = queue.Queue() # queue
+    for i in range(0, eleNum):
+        stringArray = file.readline().split()
+        process = Process(stringArray[0], int(stringArray[1]))
+        tasks.put(process)
+
+    CPUTime = 0 #clock ticks
+    while not tasks.empty(): # while have processes
+        CPUTime += 1 #OVERHEAD for selecting a program (running dispatcher)
+        process = tasks.get() # get next process on queue
+        CPUTime += 3 #OVERHEAD for loading a process
+        output += ("{0} \t{1} put on processor.\n".format(CPUTime, process.name))
+        CPUTime += process.time if process.time < TASK_TIME_LIMIT else TASK_TIME_LIMIT
+        process.time -= TASK_TIME_LIMIT
+        if process.time > 0: # if process is not done
+            tasks.put(process) # put it back on the queue
+            CPUTime += 3 #OVERHEAD for saving an incomplete process
+            output += ("{0} \t{1} removed from the processor.".format(CPUTime, process.name) + " Time left for process: {0}\n".format(process.time))
+        else:
+            output += ("{0} \t{1} removed from the processor.".format(CPUTime, process.name) + " {0} is complete.\n".format(process.name))
+
+    output += ("Complete time is {0}\n".format(CPUTime))
+    
+    with open('output.txt', 'w') as outFile:
+        outFile.write(output)
