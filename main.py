0d3430462544cec5ea6bcb4f2c6b4f406c525074
nona <iKonnyaku40@yahoo.co.jp>
2018-09-26 13:00:24 -0400

    output file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Process:
         self.name = name
         self.time = time
 
+output = ''
 
 with open('test.txt','r') as file:
     ele = file.readline().split()
@@ -25,7 +26,13 @@ with open('test.txt','r') as file:
         process.time -= TASK_TIME_LIMIT
         if process.time > 0:
             tasks.put(process)
+            output += "{0:<5} {1:>10}: Time left for process: {2:>5} \n".format(process.name, CPUTime, process.time)
             print("{0:<5} {1:>10}: Time left for process: {2:>5}".format(process.name, CPUTime, process.time))
         else:
+            output += "{0:<5} {1:>10}: {2:<5} is complete \n".format(process.name, CPUTime, process.name)
             print("{0:<5} {1:>10}: {2:<5} is complete".format(process.name, CPUTime, process.name))
     print("Complete time is {0}".format(CPUTime))
+
+with open('output.txt', 'a') as outFile:
+    # outFile.del('') # i will fix it 
+    outFile.write(output)
