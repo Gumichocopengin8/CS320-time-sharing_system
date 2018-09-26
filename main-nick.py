b448a85b4f75e5998a0717d774cb027d64590517
Smith-N <43383992+Smith-N@users.noreply.github.com>
2018-09-26 12:24:24 -0400

    Add files via upload

---
+++ main-nick.py
@@ -0,0 +1,32 @@
+import queue
+
+
+class Process:
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
+
+
+with open('test.txt','r') as file:
+    ele = file.readline().split()
+    eleNum = int(ele[0]) # number of process
+    TASK_TIME_LIMIT = int(ele[1]) # quantum
+
+    tasks = queue.Queue() # queue
+    for i in range(0, eleNum):
+        stringArray = file.readline().split()
+        process = Process(stringArray[0], int(stringArray[1]))
+        tasks.put(process)
+
+    CPUTime = 0
+    while not tasks.empty():
+        process = tasks.get()
+        CPUTime += process.time if process.time < TASK_TIME_LIMIT else TASK_TIME_LIMIT
+        process.time -= TASK_TIME_LIMIT
+        if process.time > 0:
+            tasks.put(process)
+            print("{0} {1}".format(process.name, CPUTime) + " Time left for process: {0}".format(process.time))
+        else:
+            print("{0} {1}".format(process.name, CPUTime) + " \n{0} is complete.".format(process.name))
+
+    print("Complete time is {0}".format(CPUTime))
