e05377ee0c982b0bcfb40180a8928cb7faf11921
nona <iKonnyaku40@yahoo.co.jp>
2018-09-26 21:27:17 -0400

    improvement and make code readable

--- main-final.py
+++ main-final.py
@@ -36,16 +36,15 @@ with open('test.txt','r') as file:
         CPUTime += 1 #OVERHEAD for selecting a program (running dispatcher)
         process = tasks.get() # get next process on queue
         CPUTime += 3 #OVERHEAD for loading a process
-        output += ("{0} \t{1} put on processor.\n".format(CPUTime, process.name))
+        output += ("{0:>5} {1:>10}: put on processor \n".format(CPUTime, process.name))
         CPUTime += process.time if process.time < TASK_TIME_LIMIT else TASK_TIME_LIMIT
         process.time -= TASK_TIME_LIMIT
         if process.time > 0: # if process is not done
             tasks.put(process) # put it back on the queue
             CPUTime += 3 #OVERHEAD for saving an incomplete process
-            output += ("{0} \t{1} removed from the processor.".format(CPUTime, process.name) + " Time left for process: {0}\n".format(process.time))
+            output += "{1:>5} {0:>10}: Time left for process: {2:>5} \n".format(process.name, CPUTime, process.time)
         else:
-            output += ("{0} \t{1} removed from the processor.".format(CPUTime, process.name) + " {0} is complete.\n".format(process.name))
-
+            output += "{1:>5} {0:>10}: {2} is complete \n".format(process.name, CPUTime, process.name)
     output += ("Complete time is {0}\n".format(CPUTime))
     
     with open('output.txt', 'w') as outFile:
