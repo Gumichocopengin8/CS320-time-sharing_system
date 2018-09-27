5a589378044da95448f35890633a95964e977f39
nona <iKonnyaku40@yahoo.co.jp>
2018-09-27 12:19:00 -0400

    change variable name

--- main-final.py
+++ main-final.py
@@ -20,7 +20,7 @@ class Process:
 
 
 with open('test.txt','r') as file: # read file
-    output = '{0:>5} {1:>10} {2:>5} \n\n'.format('Time', 'PID', 'log') # for output.txt file
+    auditLog = '{0:>5} {1:>10} {2:>5} \n\n'.format('Time', 'PID', 'log') # for auditLog.txt file
     ele = file.readline().split()
     eleNum = int(ele[0]) # number of process
     TASK_TIME_LIMIT = int(ele[1]) 
@@ -36,17 +36,17 @@ with open('test.txt','r') as file: # read file
         CPUTime += 1 #OVERHEAD for selecting a program (running dispatcher)
         process = tasks.get() # get next process on queue
         CPUTime += 3 #OVERHEAD for loading a process
-        output += ("{0:>5} {1:>10}: put on processor \n".format(CPUTime, process.name))
+        auditLog += ("{0:>5} {1:>10}: put on processor \n".format(CPUTime, process.name))
         CPUTime += process.time if process.time < TASK_TIME_LIMIT else TASK_TIME_LIMIT
         process.time -= TASK_TIME_LIMIT
         if process.time > 0: # if process is not done
             tasks.put(process) # put it back on the queue
             CPUTime += 3 #OVERHEAD for saving an incomplete process
-            output += "{1:>5} {0:>10}: Time left for process: {2:>5} \n".format(process.name, CPUTime, process.time)
+            auditLog += "{1:>5} {0:>10}: Time left for process: {2:>5} \n".format(process.name, CPUTime, process.time)
         else:
-            output += "{1:>5} {0:>10}: complete \n".format(process.name, CPUTime)
+            auditLog += "{1:>5} {0:>10}: complete \n".format(process.name, CPUTime)
 
-    output += ("Complete time is {0}\n".format(CPUTime))
+    auditLog += ("Complete time is {0}\n".format(CPUTime))
     
-    with open('output.txt', 'w') as outFile: # output results
-        outFile.write(output)
+    with open('auditLog.txt', 'w') as outFile: # auditLog results
+        outFile.write(auditLog)
