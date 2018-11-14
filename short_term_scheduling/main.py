4f94a231c5b8f43448652da8248f54ea8fa6202f
unWyse <kyle.d.wyse@gmail.com>
2018-11-14 12:46:31 -0500

    Still not working

---
+++ short_term_scheduling/main.py
@@ -0,0 +1,59 @@
+# Keita Nonaka
+# Nick Smith
+# Kyle Wyse
+# Koki Omori
+# 30 Sep 2018
+# OS Simulation
+
+# This program Simulates a Time Sharing Operating System
+
+# It takes an input file 'input.txt.' to run. The format of the input file should be as follows:
+# <number of tasks> <CPU time limit>
+# <job1 name> <job1 total time>
+# <jobN name> <jobN total time>
+
+# It will also provide an output file named 'auditLog.txt'
+
+
+import queue
+
+class Process:
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
+        #self.turnTime = 0
+
+with open('test.txt','r') as file: # read file
+    startTime = {}
+    endTime = {}
+    print('Hello')
+    auditLog = '{0:>5} {1:>10} {2:>5} \n\n'.format('Time', 'PID', 'log') # for output.txt file
+    ele = file.readline().split()
+    eleNum = int(ele[0]) # number of process
+    TASK_TIME_LIMIT = int(ele[1]) 
+
+    tasks = queue.Queue() # add all tasks to queue
+    for i in range(0, eleNum):
+        stringArray = file.readline().split()
+        process = Process(stringArray[0], int(stringArray[1]))
+        tasks.put(process)
+
+    CPUTime = 0 #clock ticks
+    while not tasks.empty(): # while have processes
+        CPUTime += 1 #OVERHEAD for selecting a program (running dispatcher)
+        process = tasks.get() # get next process on queue
+        CPUTime += 3 #OVERHEAD for loading a process
+        auditLog += ("{0:>5} {1:>10}: put on processor \n".format(CPUTime, process.name))
+        CPUTime += process.time if process.time < TASK_TIME_LIMIT else TASK_TIME_LIMIT
+        process.time -= TASK_TIME_LIMIT
+        #if process.time > 0: # if process is not done
+        #    tasks.put(process) # put it back on the queue
+        #    CPUTime += 3 #OVERHEAD for saving an incomplete process
+        #    auditLog += "{1:>5} {0:>10}: Time left for process: {2:>5} \n".format(process.name, CPUTime, process.time)
+        #else:
+        auditLog += "{1:>5} {0:>10}: complete \n".format(process.name, CPUTime)
+
+    auditLog += ("Complete time is {0}\n".format(CPUTime))
+    
+    with open('auditLog.txt', 'w') as outFile: # output results
+        outFile.write(auditLog)
