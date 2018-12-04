69eaa75fd718bead52cc720c6e3aab114c424524
nona <iKonnyaku40@yahoo.co.jp>
2018-12-03 22:10:55 -0500

    minor fixes

--- short_term_scheduling/main3.py
+++ short_term_scheduling/main3.py
@@ -3,15 +3,14 @@
 # Kyle Wyse # COO
 # Koki Omori # CFO
 # 30 Sep 2018
-# Updated 29 Nov 2018
+# Updated 3 Dec 2018
 # OS Simulation
 
 # This program Simulates a Time Sharing Operating System
-# It takes an input file 'input.txt.' to run. The format of the input file should be as follows:
+# It takes an input file 'input.txt.' to run and produce an output file named 'auditLog.txt'. The format of the input file should be as follows:
 # <number of tasks> <CPU time limit> !!! code no longer uses this but has not been changed to account for that. therefore there are "dummy" values in the input file
 # <job1 name> <job1 total time>
 # <jobN name> <jobN total time>
-# It will also provide an output file named 'auditLog.txt'
 
 import queue
 
@@ -21,11 +20,11 @@ class Process:
         self.time = self.servT = time
         self.waitT = 0
 
-with open('test.txt','r+') as file, open('auditLog.txt','w') as outFile:
+with open('test.txt','r') as file, open('auditLog.txt','w') as outFile:
     auditLog = '{0:>5} {1:>10} {2:>5} \n\n'.format('Time', 'PID', 'log') # for output.txt file
-    data = []
     CPUTime = totTurnTime = totNormTurnTime = 0 
     tasks = queue.PriorityQueue()
+    data = []
     eleNum = int(file.readline().split()[0]) # number of process
 
     for _ in range(eleNum):
