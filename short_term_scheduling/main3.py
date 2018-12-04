d8d4a4f39c7d10aaf314c3af5d1d76428d606408
nona <iKonnyaku40@yahoo.co.jp>
2018-12-03 21:52:58 -0500

    make code simpler

--- short_term_scheduling/main3.py
+++ short_term_scheduling/main3.py
@@ -18,47 +18,34 @@ import queue
 class Process:
     def __init__(self, name, time):
         self.name = name
-        self.time = time
-        self.waitT = 0 # new
-        self.servT = time #new
+        self.time = self.servT = time
+        self.waitT = 0
 
-with open('test.txt','r') as file: # read file
+with open('test.txt','r+') as file, open('auditLog.txt','w') as outFile:
     auditLog = '{0:>5} {1:>10} {2:>5} \n\n'.format('Time', 'PID', 'log') # for output.txt file
     data = []
-    tasks = queue.Queue()
-    ele = file.readline().split()
-    eleNum = int(ele[0]) # number of process
-    TASK_TIME_LIMIT = int(ele[1])
+    CPUTime = totTurnTime = totNormTurnTime = 0 
+    tasks = queue.PriorityQueue()
+    eleNum = int(file.readline().split()[0]) # number of process
 
-    for i in range(eleNum):
+    for _ in range(eleNum):
         stringArray = file.readline().split()
-        data += [Process(stringArray[0], int(stringArray[1]))]
-    data = sorted(data, key=lambda love: love.time)
+        tasks.put([int(stringArray[1]), Process(stringArray[0], int(stringArray[1]))])
 
-    for i in range(len(data)):
-        tasks.put(data[i])
-
-    CPUTime = 0 #clock ticks
     while not tasks.empty(): # while have processes
         CPUTime += 1 #OVERHEAD for selecting a program (running dispatcher)
-        process = tasks.get() # get next process on queue
+        process = tasks.get()[1] # get next process on queue
+        data += [process]
         CPUTime += 3 #OVERHEAD for loading a process
         auditLog += ("{0:>5} {1:>10}: put on processor\n".format(CPUTime, process.name))
-        process.waitT = CPUTime #new
+        process.waitT = CPUTime
         CPUTime += process.time
-        process.time -= TASK_TIME_LIMIT # is this line still necessary...?
         auditLog += "{1:>5} {0:>10}: complete\n".format(process.name, CPUTime)
     auditLog += ("Complete time is {0}\n".format(CPUTime))
 
-#new...
-    totTurnTime = 0 
-    totNormTurnTime = 0
-    for i in range(len(data)):
-        totTurnTime += data[i].waitT + data[i].servT
-        totNormTurnTime += (data[i].waitT + data[i].servT) / data[i].servT
+    for item in data:
+        totTurnTime += item.waitT + item.servT
+        totNormTurnTime += (item.waitT + item.servT) / item.servT
     auditLog += ("Average turnaround time is {0}\n".format(totTurnTime/len(data)))
     auditLog += ("Average normalized turnaround time is {0}\n".format(totNormTurnTime/len(data)))
-#...new
-
-    with open('auditLog.txt', 'w') as outFile: # output results
-        outFile.write(auditLog)
+    outFile.write(auditLog)
